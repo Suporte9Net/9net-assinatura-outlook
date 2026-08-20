99dae0c0020a7c6b20da1df1296718841c285f46
Suporte9Net <suporte-9net@9net.com.br>
2026-08-20 15:00:18 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@
 
   window.NineNetConfig = Object.freeze({
     version: "2.0.0",
-    clientId: "CLIENT_ID_PENDENTE",
-    tenantId: "TENANT_ID_PENDENTE",
-    baseUrl: "https://suporte9net.github.io/9net-assinatura-outlook",
+    clientId: "eaaf2251-f42e-4705-861f-66dd100ca29b",
+    tenantId: "e66ee0b3-9cf5-40f5-bc5a-a46efc7a8063",
+    baseUrl: "https://9net-assinatura-outlook.pages.dev",
     graphScopes: Object.freeze(["User.Read"]),
     graphUserUrl: "https://graph.microsoft.com/v1.0/me?$select=displayName,jobTitle,mail,userPrincipalName,businessPhones,faxNumber,mobilePhone,streetAddress,city,state,postalCode,country,department,officeLocation",
     websiteUrl: "https://9net.com.br/",
